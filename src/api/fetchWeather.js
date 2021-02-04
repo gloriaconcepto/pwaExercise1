a369fd516eff0b14ce77821a5493bac363ce8139
mmekut-mfon.edet <mmekut-mfon.edet@interswitchgroup.com>
2021-02-04 17:07:31 +0100

    PWA tutorial exercises added

---
+++ src/api/fetchWeather.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import { WEATHERURL } from "../constants/Api";
+
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+export const fetchWeather = async (query) => {
+    const { data } = await axios.get(WEATHERURL, {
+        params: {
+            q: query,
+            units: "metric",
+            APPID: API_KEY,
+        },
+    });
+
+    return data;
+};
---
+++ src/constants/Api.js
@@ -0,0 +1,1 @@
+export const WEATHERURL = 'https://api.openweathermap.org/data/2.5/weather';
